014e32f77d350cc6ab0174a300992b66fa87b668
Anna L. Ehrhardt <aehrhard@gmail.com>
2014-10-16 11:13:31 -0400

    asset pipeline

--- config/application.rb
+++ config/application.rb
@@ -15,8 +15,8 @@ module Portfolio
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
-config.serve_static_assets = true
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+    config.assets.paths << "#{Rails.root}/vendor/assets/fonts"
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
   end
